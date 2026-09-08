07f02dadc13771e875b514c5a85dda891efdebcf
Onnicha.piti <onnicha.piti@bumail.net>
2026-09-08 16:02:36 +0700

    in class1

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -8,21 +8,56 @@ namespace Assignment
         public void Start()
         {
             // LCT01_SyntaxList();
-            // LCT02_SyntaxLinkedList();
+            LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+            LCT04_SyntaxDictionary();
         }
 
         #region Lecture
 
         public void LCT01_SyntaxList()
         {
-            throw new System.NotImplementedException();
+            List<string> lists = new List<string>();
         }
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.AddLast("Node 1");
+            linkedList.AddLast("Node 2");
+            linkedList.AddFirst("Node 0");
+            PrintLinkList(linkedList);
+
+            LinkedListNode<string> frisNode = linkedList.First;
+            Debug.Log("first: " + frisNode.Value);
+            LinkedListNode<string> lastNode = linkedList.Last;
+            Debug.Log("last: " + lastNode.Value);
+            LinkedListNode<string> node1 = linkedList.Find("Node 1");
+            Debug.Log("node: " + node1.Value);
+            Debug.Log(node1.Previous.Value);
+            Debug.Log(node1.Next.Value);
+            if(frisNode.Previous == null) { Debug.Log("firstNode.Previous is null"); }
+            if(lastNode.Previous == null) { Debug.Log("lastNode.Next is null"); }
+
+            linkedList.AddAfter(node1, "Node 1.5");
+            linkedList.AddBefore(node1, "Node 0.5");
+            PrintLinkList(linkedList);
+
+            linkedList.RemoveFirst();
+            PrintLinkList(linkedList);
+            linkedList.Remove("Node 2");
+            PrintLinkList(linkedList);
+            linkedList.Clear();
+            PrintLinkList(linkedList);
+        }
+
+        void PrintLinkList(LinkedList<string> linkedList)
+        {
+            Debug.Log("----- linkedList -----");
+            foreach (string s in linkedList)
+            {
+                Debug.Log(s);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,7 +67,41 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<int,string> dictionary = new Dictionary<int,string>();
+            dictionary.Add(1, "Apple");
+            dictionary.Add(2, "Banana");
+            dictionary[3] = "Cherry";
+
+            //Debug.Log(dictionary[4]);
+            int keytocheck = 1;
+            bool hasKey = dictionary.ContainsKey(keytocheck);
+            Debug.Log($"has key {keytocheck}:{hasKey}");
+            if(hasKey)
+            {
+                Debug.Log(dictionary[keytocheck]);
+            }
+
+            foreach (int k in dictionary.Keys)
+            {
+                Debug.Log(k);
+            }
+
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
+
+            dictionary.Remove(1);
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
+
+            dictionary.Clear();
+            foreach (string s in dictionary.Values)
+            {
+                Debug.Log(s);
+            }
         }
 
         #endregion
